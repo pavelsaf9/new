f7c512f290b792e8d7aafdac7b6efb66ce9e26f0
Vasya <pavelsaf9@gmail.com>
2023-03-10 21:24:18 +0900

    Vasya

--- lesson 8/lesson 8(6).py
+++ lesson 8/lesson 8(6).py
@@ -5,10 +5,9 @@ def guess_number():
     target = random.randint(1, 10)
 
     for i in range(3):
-        guess = input("Введите число от 1 до 10: ")
+        guess = int(input("Введите число от 1 до 10: "))
 
         try:
-            guess = int(guess)
             if guess < 1 or guess > 10:
                 raise ValueError("Ошибка! Вы ввели не число")
 
